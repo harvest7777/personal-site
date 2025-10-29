c1a1779f80476ec09de4d1eaf31ec6a608ea29fb
harvest7777 <333harvest.moon333@gmail.com>
2025-10-28 20:17:10 -0700

    href scroll top

--- app/sections-container.tsx
+++ app/sections-container.tsx
@@ -8,7 +8,7 @@ export default function SectionsContainer({
   id: string;
 }) {
   return (
-    <section id={id} className="w-full scroll-mt-24">
+    <section id={id} className="w-full scroll-mt-20">
       <h1 className=" w-full text-left">{heading}</h1>
       <div className="w-full h-px bg-foreground/20 my-2"></div>
       <div className="flex flex-col gap-y-4">{children}</div>
